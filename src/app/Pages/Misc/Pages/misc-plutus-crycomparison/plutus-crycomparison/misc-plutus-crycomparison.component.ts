a9a28544f62ee9f4b287f05b6ce5befe79fbd062
StarLinez <StarLines.github@gmail.com>
2024-08-11 21:00:44 +0200

    Changes to new plutus thing

--- src/app/Pages/Misc/Pages/misc-plutus-crycomparison/plutus-crycomparison/misc-plutus-crycomparison.component.ts
+++ src/app/Pages/Misc/Pages/misc-plutus-crycomparison/plutus-crycomparison/misc-plutus-crycomparison.component.ts
@@ -16,13 +16,13 @@ export class MiscPlutusCryComparisonComponent implements OnInit, OnDestroy {
   url: string = 'https://api.coingecko.com/api/v3/simple/price?ids=pluton&vs_currencies=eur%2Cgbp';
   url2: string = 'https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=eur%2Cgbp';
   pluPrice: Pluton = {
-    eur: 0,
-    gbp: 0
+    eur: 2.25,
+    gbp: 1.93
   };
 
   tetherPrice: Tether = {
-    eur: 0.921946,
-    gbp: 0.776655
+    eur: 0.9159,
+    gbp: 0.7843
   };
 
   stackingTiersNew: PlutusStackingTierNew[] = PlutusWpJson.stackingTiers;
@@ -30,8 +30,6 @@ export class MiscPlutusCryComparisonComponent implements OnInit, OnDestroy {
 
   selectedStackingTierNew = this.stackingTiersNew[0];
   selectedStackingTierOld = this.stackingTiersOld[0];
-  selectedStackingTierIndexNew = 0;
-  selectedStackingTierIndexOld = 0;
 
   heldPluCount: number;
   currencySymbol: string = "€";
@@ -45,26 +43,25 @@ export class MiscPlutusCryComparisonComponent implements OnInit, OnDestroy {
   eligibleSpendNew: number = 0;
   eligibleSpendOld: number = 0;
 
+  countAdditionalBenefits: boolean = true;
+  additionalBenefitsNew: number = 0;
+  additionalBenefitsOld: number = 0;
+
   monthlyCashbackValueNew: number = 0;
   monthlyCashbackValueOld: number = 0;
   monthlyPerkValueNew: number = 0;
   monthlyPerkValueOld: number = 0;
   monthlyCryValueNew: number = 0;
   monthlyCryValueOld: number = 0;
-  monthlyCryValueYear2: number = 0;
 
   totalMonthlyValueNew: number = 0;
   totalMonthlyValueOld: number = 0;
   
+  yearlyStackValuesNew: number[] = [0, 0, 0, 0, 0];
+  yearlyStackValuesOld: number[] = [0, 0, 0, 0, 0];
+
   year1TotalValueNew: number = 0;
   year1TotalValueOld: number = 0;
-  year2TotalValueNew: number = 0;
-  year2TotalValueOld: number = 0;
-
-  totalValue: number[] = [0, 0, 0];
-  totalActualValue: number[] = [0, 0, 0];
-  totalValueMinusCost: number[] = [0, 0, 0];
-  totalOriginalBenefits: number[] = [0, 0, 0];
 
   constructor(private titleService: Title, private metaService: Meta, private http: HttpClient) {
   }
@@ -91,8 +88,8 @@ export class MiscPlutusCryComparisonComponent implements OnInit, OnDestroy {
     }
 
     // warning this fetch is not waited on, so if used for calculations, run the calculation again in this function.
-    this.fetchTetherPrice();
-    this.fetchPluPrice();
+    //this.fetchTetherPrice();
+    //this.fetchPluPrice();
 
     this.calculate();
   }
@@ -145,115 +142,128 @@ export class MiscPlutusCryComparisonComponent implements OnInit, OnDestroy {
   }
 
   calculate() {
-    this.calculateTierNew();
-    this.calculateTierOld();
+    this.selectedStackingTierNew = this.calculateTierNew(this.heldPluCount);
+    this.selectedStackingTierOld = this.calculateTierOld(this.heldPluCount);
 
-    this.calculateCashbackRates();
-    this.calculatePerkCounts();
-    this.calculateCryRate();
-    this.calculateEligibleSpends();
+    this.cashbackRateNew = this.calculateCashbackRateNew(this.selectedStackingTierNew, this.heldPluCount); 
+    this.cashbackRateOld = this.selectedStackingTierOld.cashbackPercentage; 
 
-    this.calculateMonthlyCashback(); //TODO funky noob stuffs
-    this.calculateMonthlyPerkValue();
-    this.calculateMonthlyCryValue();
-
-    this.calculateTotalMonthlyValue();
-    this.calculateYear1TotalValue();
-    this.calculateMonthlyCryValueYear2();
-    this.calculateYear2TotalValue();
-  }
-
-  calculateTierNew() {
-    if (this.heldPluCount == null) {
-      this.selectedStackingTierNew = this.stackingTiersNew[0];
-      this.selectedStackingTierIndexNew = 0;
-    } else if (this.heldPluCount >= 50000) {
-      this.selectedStackingTierNew = this.stackingTiersNew[this.stackingTiersNew.length - 1];
-      this.selectedStackingTierIndexNew = this.stackingTiersNew.length - 1;
-    } else {
-        for (let i = 0; i < this.stackingTiersNew.length; i++) {
-          if (this.stackingTiersNew[i].pluRequired > this.heldPluCount) {
-            this.selectedStackingTierNew = this.stackingTiersNew[i - 1];
-            this.selectedStackingTierIndexNew = i - 1;
-            break;
-          }
-        }
-    }
-  }
-
-  calculateTierOld() {
-    if (this.heldPluCount == null) {
-      this.selectedStackingTierOld = this.stackingTiersOld[0];
-      this.selectedStackingTierIndexOld = 0;
-    } else if (this.heldPluCount >= 3000) {
-      this.selectedStackingTierOld = this.stackingTiersOld[this.stackingTiersOld.length - 1];
-      this.selectedStackingTierIndexOld = this.stackingTiersOld.length - 1;
-    } else {
-        for (let i = 0; i < this.stackingTiersOld.length; i++) {
-          if (this.stackingTiersOld[i].pluRequired > this.heldPluCount) {
-            this.selectedStackingTierOld = this.stackingTiersOld[i - 1];
-            this.selectedStackingTierIndexOld = i - 1;
-            break;
-          }
-        }
-    }
-  }
-
-  calculateCashbackRates() {
-    this.cashbackRateOld = this.selectedStackingTierOld.cashbackPercentage;
-
-    if (this.selectedStackingTierNew.name !== "None") {
-      if (this.selectedStackingTierNew.cashbackPercentage === this.stackingTiersNew[this.stackingTiersNew.length - 1].cashbackPercentage || this.stackingTiersNew[this.selectedStackingTierIndexNew + 1].cashbackPercentage < 4) {
-        this.cashbackRateNew = this.selectedStackingTierNew.cashbackPercentage;
-      } else {
-        var pluToNextTier = this.stackingTiersNew[this.selectedStackingTierIndexNew + 1].pluRequired - this.stackingTiersNew[this.selectedStackingTierIndexNew].pluRequired;
-        var extraPlu = this.heldPluCount - this.selectedStackingTierNew.pluRequired;
-        var incrementalPercentage = Math.floor(extraPlu / pluToNextTier * 10) / 10
-        this.cashbackRateNew = this.selectedStackingTierNew.cashbackPercentage + incrementalPercentage;
-      }
-      return;
-    }
-
-    
-    this.cashbackRateNew = this.selectedStackingTierNew.cashbackPercentage;
-  }
-
-  calculatePerkCounts() {
-    this.perkCountNew = this.selectedStackingTierNew.perkCount;  
+    this.perkCountNew = this.selectedStackingTierNew.perkCount;
     this.perkCountOld = this.selectedStackingTierOld.perkCount;
-  }
 
-  calculateCryRate() {
-    var cryRate = 0;
-    if (this.cashbackRateNew == 3) {
-      cryRate = this.selectedStackingTierNew.cryRate;
-    } else {
-      cryRate = this.cashbackRateNew;
-    }
-    
-    this.cryRateNew = cryRate;
+    this.cryRateNew = this.calculateCryRate(this.cashbackRateNew, this.selectedStackingTierNew);
     this.cryRateOld = 0;
-  }
 
-  calculateEligibleSpends() {
-    if (this.heldPluCount === null || isNaN(this.heldPluCount)) {
-      this.eligibleSpendNew = 0;
-    } else {
-      this.eligibleSpendNew = this.heldPluCount;
-    }
+    this.eligibleSpendNew = this.calculateEligibleSpendNew(this.heldPluCount);
     this.eligibleSpendOld = 0;
-  }
 
-  calculateMonthlyCashback() {
-    this.monthlyCashbackValueNew = this.eligibleSpendNew * (this.cashbackRateNew / 100);
-    this.monthlyCashbackValueOld = this.eligibleSpendOld * (this.cashbackRateOld / 100);
-  }
+    this.monthlyCashbackValueNew = this.calculateMonthlyCashback(this.eligibleSpendNew, this.cashbackRateNew); //TODO funky noob stuffs
+    this.monthlyCashbackValueOld = this.calculateMonthlyCashback(this.eligibleSpendOld, this.cashbackRateOld);
 
-  calculateMonthlyPerkValue() {
+    this.additionalBenefitsNew = this.calculateAdditionalBenefits(this.monthlyCashbackValueNew, this.selectedStackingTierNew);
+    this.additionalBenefitsOld = 0;
+
     this.monthlyPerkValueNew = this.perkCountNew * 10;
     this.monthlyPerkValueOld = this.perkCountOld * 10;
+
+    this.calculateMonthlyCryValue();
+
+    this.totalMonthlyValueNew = this.calculateTotalMonthlyValue(this.monthlyCashbackValueNew, this.monthlyPerkValueNew, this.monthlyCryValueNew, this.additionalBenefitsNew);
+    this.totalMonthlyValueOld = this.calculateTotalMonthlyValue(this.monthlyCashbackValueOld, this.monthlyPerkValueOld, this.monthlyCryValueOld, this.additionalBenefitsOld);
+
+    this.calculateYear1TotalValue();
+
+    this.calculateRewardsOver5YearsWithCry();
+    this.calculateRewardsOver5YearsWithoutCry();
   }
 
+  calculateTierNew(heldPlu: number) {
+    if (heldPlu == null || heldPlu == undefined) {
+      return this.stackingTiersNew[0];
+    } else if (heldPlu >= 50000) {
+      return this.stackingTiersNew[this.stackingTiersNew.length - 1];
+    } else {
+        for (let i = 0; i < this.stackingTiersNew.length; i++) {
+          if (this.stackingTiersNew[i].pluRequired > heldPlu) {
+            return this.stackingTiersNew[i - 1];
+          }
+        }
+    }
+  }
+
+  calculateTierOld(heldPlu: number) {
+    if (heldPlu == null) {
+      return this.stackingTiersOld[0];
+    } else if (heldPlu >= 3000) {
+      return this.stackingTiersOld[this.stackingTiersOld.length - 1];
+    } else {
+        for (let i = 0; i < this.stackingTiersOld.length; i++) {
+          if (this.stackingTiersOld[i].pluRequired > heldPlu) {
+            return this.stackingTiersOld[i - 1];
+          }
+        }
+    }
+  }
+
+  calculateCashbackRateNew(stackingTier: PlutusStackingTierNew, pluCount: number) {
+    var selectedStackingTierIndex = this.stackingTiersNew.indexOf(stackingTier);
+
+    if (stackingTier.name !== "None") {
+      if (stackingTier.cashbackPercentage === this.stackingTiersNew[this.stackingTiersNew.length - 1].cashbackPercentage || this.stackingTiersNew[selectedStackingTierIndex + 1].cashbackPercentage < 4) {
+        return stackingTier.cashbackPercentage;
+      } else {
+        var pluToNextTier = this.stackingTiersNew[selectedStackingTierIndex + 1].pluRequired - this.stackingTiersNew[selectedStackingTierIndex].pluRequired;
+        var extraPlu = pluCount - stackingTier.pluRequired;
+        var incrementalPercentage = Math.floor(extraPlu / pluToNextTier * 10) / 10
+        return stackingTier.cashbackPercentage + incrementalPercentage;
+      }
+    }
+    
+    return stackingTier.cashbackPercentage;
+  }
+
+  calculateCryRate(cashbackRate: number, stackingTier: PlutusStackingTierNew) {
+    var cryRate = 0;
+    // anything with a cashbackrate of 3 will not have incremental cryrates or a cry rate above 3 so the stackingTiers default cryrate is chosen
+    // anything with a cashback rate above 3 will have the same cry rate as their cashback %.
+    if (cashbackRate == 3) {
+      return stackingTier.cryRate;
+    } else {
+      return cashbackRate;
+    }
+  }
+
+  calculateEligibleSpendNew(heldPlu: number) {
+    if (heldPlu === null || isNaN(heldPlu)) {
+      return 0;
+    } else {
+      return heldPlu;
+    }
+  }
+
+  calculateAdditionalBenefits(monthlyCashbackValue: number, stackingTier: PlutusStackingTierNew) {
+    var totalAdditionalBenefits = 0;
+
+    if(monthlyCashbackValue > (stackingTier.doubleRewards * 20)) {
+      totalAdditionalBenefits += stackingTier.doubleRewards * 20;
+    } else {
+      totalAdditionalBenefits += monthlyCashbackValue;
+    }
+  
+    if (this.currencySymbol === "€") {
+      totalAdditionalBenefits = stackingTier.goldenTickets * 20 * this.tetherPrice.eur;
+    } else {
+      totalAdditionalBenefits = stackingTier.goldenTickets * 20 * this.tetherPrice.gbp;
+    }
+
+    return totalAdditionalBenefits;
+  } 
+
+  calculateMonthlyCashback(eligibleSpend: number, cashbackRate: number) {
+    return eligibleSpend * (cashbackRate / 100);
+  }
+
+  // TODO: don't use this instead take the first item from the eventual array that will be made for year 1-5
   calculateMonthlyCryValue() {
     var intermediateMonthlyValue = this.monthlyCashbackValueNew + this.monthlyPerkValueNew;
     var pluPrice = 0;
@@ -265,15 +275,18 @@ export class MiscPlutusCryComparisonComponent implements OnInit, OnDestroy {
 
     // to prevent it from calculating NaN check if heldPluCount is not null
     if(this.heldPluCount != null) {
-      this.monthlyCryValueNew = ((this.heldPluCount * pluPrice / 12) + intermediateMonthlyValue) * (this.cryRateNew / 100) * 2;
+      this.monthlyCryValueNew = ((this.heldPluCount / 12) * pluPrice) * (this.cryRateNew / 100) * 2;
     } else {
       this.monthlyCryValueNew = intermediateMonthlyValue * (this.cryRateNew / 100) * 2;
     }
    }
   
-  calculateTotalMonthlyValue() {
-    this.totalMonthlyValueNew = this.monthlyCashbackValueNew + this.monthlyPerkValueNew + this.monthlyCryValueNew;
-    this.totalMonthlyValueOld = this.monthlyCashbackValueOld + this.monthlyPerkValueOld + this.monthlyCryValueOld;
+  calculateTotalMonthlyValue(monthlyCashbackValue, monthlyPerkValue, monthlyCryValue, additionalBenefits) {
+    var total = monthlyCashbackValue + monthlyPerkValue + monthlyCryValue;
+    if (this.countAdditionalBenefits){
+      total += additionalBenefits;
+    }
+    return total;
   }
 
   calculateYear1TotalValue() {
@@ -281,8 +294,11 @@ export class MiscPlutusCryComparisonComponent implements OnInit, OnDestroy {
     this.year1TotalValueOld = (this.totalMonthlyValueOld * 12)
   }
 
-  calculateMonthlyCryValueYear2 () {
-    var intermediateMonthlyValue = this.monthlyCashbackValueNew + this.monthlyPerkValueNew;
+  calculateRewardsOver5YearsWithCry() {
+    if(this.heldPluCount == null || this.heldPluCount == undefined || this.heldPluCount == 0) {
+      return; //exit out of the function as otherwise there will be dividing by zero.
+    }
+
     var pluPrice = 0;
     if (this.currencySymbol === "€") {
       pluPrice = this.pluPrice.eur;
@@ -290,20 +306,56 @@ export class MiscPlutusCryComparisonComponent implements OnInit, OnDestroy {
       pluPrice = this.pluPrice.gbp;
     }
 
-    var heldPluAfter1Year = 0;
-    // to prevent a divide by zero
-    if(this.year1TotalValueNew != 0) {
-      heldPluAfter1Year = (this.year1TotalValueNew / pluPrice) + this.heldPluCount;
+    var totalStack = this.heldPluCount; //total PLU held to which monthly cashback, perks are added constantly
+    var startingStackYear = totalStack; // stack amount used to calculate cry
+    var totalCryValue = 0; // totalValue gained in cry over all years (might see about returning an array of 5 values instead)
+    var cryValueYearly = 0; // totalCry per year, this counter is reset for every year calculated after being added to the totalstack.
+
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 12; j++) {
+        var stackingTier = this.calculateTierNew(totalStack);
+        var cashbackRate = this.calculateCashbackRateNew(stackingTier, totalStack);
+        var perkCount = stackingTier.perkCount;
+        var eligibleSpend = this.calculateEligibleSpendNew(totalStack);
+        var cryRate = this.calculateCryRate(cashbackRate, stackingTier);
+
+        var monthlyCashback = this.calculateMonthlyCashback(eligibleSpend, cashbackRate);
+        var monthlyPerkValue = perkCount * 10;
+
+        // add the PLU count for the rewards value
+        totalStack += (monthlyCashback + monthlyPerkValue) / pluPrice;
+
+        // calculating cryvalue on 1/12 of the starting stack of the year so that the multiplier adjusts on a monthly basis based on the increasing PLU held (from rewards being earned)
+        // taking the cryrate + (the cryrate * years) to add on the bonus of holding for multiple years without resetting it
+        cryValueYearly += ((startingStackYear / 12) * pluPrice) * ((cryRate + (cryRate * (i + 1))) / 100); 
+      }
+      totalCryValue += cryValueYearly;
+      totalStack += (cryValueYearly / pluPrice);
+      this.yearlyStackValuesNew[i] = totalStack;
+      cryValueYearly = 0;
+      startingStackYear = totalStack;
+    }
+  }
+
+  calculateRewardsOver5YearsWithoutCry() {
+    if (this.heldPluCount == null || this.heldPluCount == undefined) {
+      return
     }
 
-    this.monthlyCryValueYear2 = ((heldPluAfter1Year * pluPrice / 12) + intermediateMonthlyValue) * (this.cryRateNew / 100) * 3;
+    let pluPrice = 0;
+
+    if (this.currencySymbol === "€") {
+      pluPrice = this.pluPrice.eur;
+    } else {
+      pluPrice = this.pluPrice.gbp;
+    }
+
+    for (let i = 0; i < 5; i++) {
+      this.yearlyStackValuesOld[i] = this.heldPluCount + (this.totalMonthlyValueOld / pluPrice * 12 * (i + 1));
+    }
   }
 
-  calculateYear2TotalValue() {
-    this.year2TotalValueNew = this.year1TotalValueNew + ((this.monthlyCryValueYear2 + this.monthlyPerkValueNew + this.monthlyCashbackValueNew) * 12);
 
-    this.year2TotalValueOld = (this.totalMonthlyValueOld * 24);
-  }
 
   changeCurrency() {
     if (this.currencySymbol === "€") {
